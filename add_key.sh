9098660f22643d89c65001a894b1a41cc4426013
cda75 <cda75@users.noreply.github.com>
2017-06-15 15:31:25 +0300

    Create add_key.sh

--- add_key.sh
+++ add_key.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 useradd -m -d /home/dimka -s /bin/bash dimka
-# echo "dimka:merlin" | chpasswd
 usermod -aG sudo dimka
 mkdir -p /home/dimka/.ssh
-cat ansible_key.pub > /home/dimka/.ssh/authorized_keys
+cat ansible_key.pub >> /home/dimka/.ssh/authorized_keys
 echo "dimka ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 chown -R dimka:dimka /home/dimka
 chmod 700 /home/dimka/.ssh/
@@ -11,4 +10,4 @@ chmod 600 /home/dimka/.ssh/authorized_keys
 echo "AuthorizedKeysFile      %h/.ssh/authorized_keys" >> /etc/ssh/sshd_config
 service sshd restart
 cd ../
-rm -rf ansible-host/
+rm -rf anh/
