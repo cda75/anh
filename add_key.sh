6cc767f64ab7b9b6003f66408e8f2990a363a339
cda75 <dchestnov@mail.ru>
2017-06-15 13:13:45 +0300

    all

--- add_key.sh
+++ add_key.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
-useradd dimka -d /home/dimka -s /bin/bash -m 
+useradd -m -d /home/dimka -s /bin/bash dimka
 echo "dimka:merlin" | chpasswd
 usermod -aG sudo dimka
+mv ansible_key.pub /home/dimka/key
+chmod 777 /home/dimka/key
+su - dimka
+mkdir -p .ssh
+chmod 700 .ssh
+cat key > authorized_keys
+rm -f /home/dimka/key
+chmod 600 .ssh/authorized_keys
 
