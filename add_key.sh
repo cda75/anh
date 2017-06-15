52263a33581a4883ad7b09447ec59082c53f5463
cda75 <dchestnov@mail.ru>
2017-06-15 13:37:06 +0300

    ttt

--- add_key.sh
+++ add_key.sh
@@ -4,10 +4,11 @@ echo "dimka:merlin" | chpasswd
 usermod -aG sudo dimka
 mv ansible_key.pub /home/dimka/key
 chmod 777 /home/dimka/key
-su dimka
-mkdir .ssh
-chmod 700 .ssh/
-cat key > .ssh/authorized_keys
+su - dimka
+mkdir /home/dimka/.ssh
+chmod 700 /home/dimka/.ssh/
+cat /home/dimka/key > /home/dimka/.ssh/authorized_keys
 rm -f /home/dimka/key
-chmod 600 .ssh/authorized_keys
+chmod 600 /home/dimka/.ssh/authorized_keys
+echo "dimka ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
 sudo service sshd restart
